0fd9a95e7a585a12330aae3234f0cefe8696b813
stii <tencktech@gmail.com>
2020-03-21 12:23:58 +0100

    added level-2 - question 2

--- level-2.js
+++ level-2.js
@@ -7,3 +7,15 @@ for (i = 15; i <= 25; i++) {
 }
 
 // question 2
+
+function robot() {
+  console.log("I am a function");
+}
+
+var innerFunction = robot();
+
+function outerFunction(human) {
+  outerFunction(human);
+}
+
+outerFunction(robot);
